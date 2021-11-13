dd5d7a6360a4dee716c373beae0bddf2f1d3bcbb
Mikhail Cheremuhin-Rerberg <mcheremukhin-rerberg@alfabank.ru>
2021-11-13 20:53:55 +0300

    add port env var

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
